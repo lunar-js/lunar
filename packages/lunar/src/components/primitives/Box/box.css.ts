5adc393984784279f4c0a5d22efa7237e3130fff
Shayne Preston <shayne.preston@protonmail.com>
2025-09-24 09:51:33 -0500

    clean up

--- packages/lunar/src/components/primitives/Box/box.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const box = style({
-  // Base box styles - minimal reset
-  boxSizing: 'border-box',
-});
--- packages/lunar/src/components/primitives/Box/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Utilities } from '../../../styles/utilities.css.js';
-
-export interface BoxProperties extends Utilities {
-  children?: unknown;
-  as?: string;
-  className?: string;
-}
