507441b25991332862c04663a8557353bd4151a9
Shayne Preston <shayne.preston@protonmail.com>
2025-11-11 11:44:04 -0500

    update font-size primtives

--- packages/lunar/src/tokens/primitives/typography.ts
+++ packages/lunar/src/tokens/primitives/typography.ts
@@ -31,19 +31,19 @@ export const FONT_FAMILY = {
 } as const;
 
 export const FONT_SIZE = {
-  xs: '0.75rem', // 12px
-  sm: '0.875rem', // 14px
-  base: '1rem', // 16px
-  lg: '1.125rem', // 18px
-  xl: '1.25rem', // 20px
-  '2xl': '1.5rem', // 24px
-  '3xl': '1.875rem', // 30px
-  '4xl': '2.25rem', // 36px
-  '5xl': '3rem', // 48px
-  '6xl': '3.75rem', // 60px
-  '7xl': '4.5rem', // 72px
-  '8xl': '6rem', // 96px
-  '9xl': '8rem', // 128px
+  xs: '0.9375rem', // 15px
+  sm: '1.09375rem', // 17.5px
+  base: '1.25rem', // 20px
+  lg: '1.40625rem', // 22.5px
+  xl: '1.5625rem', // 25px
+  '2xl': '1.875rem', // 30px
+  '3xl': '2.34375rem', // 37.5px
+  '4xl': '2.8125rem', // 45px
+  '5xl': '3.75rem', // 60px
+  '6xl': '4.6875rem', // 75px
+  '7xl': '5.625rem', // 90px
+  '8xl': '7.5rem', // 120px
+  '9xl': '10rem', // 160px
 } as const;
 
 export const FONT_WEIGHT = {
